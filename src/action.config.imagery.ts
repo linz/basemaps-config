d9f9d7472967504a2b3ee3f74bfa7bf9d55d9f06
Geoff Jacobsen <gjacobson@linz.govt.nz>
2020-11-05 10:30:34 +1300

    feat: Configure imagery on PR merge (#3)
    * feat: Configure imagery on PR merge
    
    * Add a build step for dry run
    
    Also sync all config files to production tag
    
    * fix aerial-3857 background color
    
    * display config file in logger
    
    * remove tag creation code
    
    tag is created by yarn version
    
    * restore push version
    
    * remove redundant env vars

--- src/action.config.imagery.ts
+++ src/action.config.imagery.ts
@@ -1,28 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { LogConfig, TileMetadataTag } from '@basemaps/shared';
-import { CommandLineAction, CommandLineStringParameter } from '@rushstack/ts-command-line';
-// FIXME GJ import { defineTagParameter } from '@basemaps/cli/build/cli/basemaps/tileset.util';
-
-/**
- * FIXME import from tile.metadata.base.ts
- */
-export function parseMetadataTag(tagInput: string | null | undefined): any | null {
-    if (tagInput == null) return null;
-    switch (tagInput) {
-        case TileMetadataTag.Head:
-        case TileMetadataTag.Production:
-        case TileMetadataTag.Beta:
-            return tagInput;
-        default:
-            if (/^pr-[0-9]+$/.test(tagInput)) return tagInput;
-            return null;
-    }
-}
+import { updateConfig } from '@basemaps/cli';
+import { defineTagParameter } from '@basemaps/cli/build/cli/basemaps/tileset.util';
+import { LogConfig, parseMetadataTag } from '@basemaps/shared';
+import { CommandLineAction, CommandLineFlagParameter, CommandLineStringParameter } from '@rushstack/ts-command-line';
 
 const imageryConfigFileRe = /^config\/imagery\/[^./]+.json$/;
 
 export class ConfigImageryImportAction extends CommandLineAction {
     private tag: CommandLineStringParameter;
+    private commit: CommandLineFlagParameter;
 
     public constructor() {
         super({
@@ -33,17 +19,17 @@ export class ConfigImageryImportAction extends CommandLineAction {
     }
 
     protected onDefineParameters(): void {
-        this.tag = this.defineStringParameter({
-            argumentName: 'TAG',
-            parameterLongName: '--tag',
-            parameterShortName: '-t',
-            description: `tag name  (options: ${['FIXME'].join(', ')} or pr-<pr_number>)`,
-            required: true,
+        defineTagParameter(this);
+        this.commit = this.defineFlagParameter({
+            parameterLongName: '--commit',
+            description: 'Commit to database',
+            required: false,
         });
     }
 
     protected async onExecute(): Promise<void> {
         const tagInput = this.tag.value!;
+        const isCommit = !!this.commit.value;
 
         const tag = parseMetadataTag(tagInput);
         if (tag == null) {
@@ -76,8 +62,11 @@ export class ConfigImageryImportAction extends CommandLineAction {
             .split(/\s+/)
             .filter((f) => imageryConfigFileRe.test(f));
 
-        for (const file of files) {
-            console.log(`DEBUG file`, file);
+        const logger = LogConfig.get();
+
+        for (const configFile of files) {
+            LogConfig.set(logger.child({ configFile }));
+            await updateConfig(configFile, tag, isCommit);
         }
     }
 }
