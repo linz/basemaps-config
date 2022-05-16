0d2a5c347e5e12eda6a2c639ea82a106a4c2bfac
Blayne Chard <bchard@linz.govt.nz>
2022-05-16 16:15:04 +1200

    ci: zoom in slightly

--- src/commands/screenshot.ts
+++ src/commands/screenshot.ts
@@ -2,11 +2,11 @@ import { GoogleTms, Nztm2000QuadTms } from '@basemaps/geo';
 import { Config, LogConfig, LogType } from '@basemaps/shared';
 import { Command, Flags } from '@oclif/core';
 import { mkdir } from 'fs/promises';
-import { chromium, Page } from 'playwright';
+import { Browser, chromium, Page } from 'playwright';
 
 const TileTest = [
-  { name: 'health3857-z5', tileMatrix: GoogleTms, location: { lat: -41.8899962, lng: 174.0492437, z: 5 }, tileSet: 'health', style: undefined },
-  { name: 'health2193-z5', tileMatrix: Nztm2000QuadTms, location: { lat: 0, lng: 0, z: 0 }, tileSet: 'aerial' },
+  { name: 'health-3857-z5', tileMatrix: GoogleTms, location: { lat: -41.8899962, lng: 174.0492437, z: 5 }, tileSet: 'health', style: undefined },
+  { name: 'health-2193-z5', tileMatrix: Nztm2000QuadTms, location: { lat: -41.8899962, lng: 174.0492437, z: 1 }, tileSet: 'aerial' },
   { name: 'topographic-3857-z5', tileMatrix: GoogleTms, location: { lat: -41.8899962, lng: 174.0492437, z: 5 }, tileSet: 'topographic', style: 'topographic' },
   { name: 'topolite-3857-z5', tileMatrix: GoogleTms, location: { lat: -41.8899962, lng: 174.0492437, z: 5 }, tileSet: 'topographic', style: 'topolite' },
   { name: 'topographic-3857-z14', tileMatrix: GoogleTms, location: { lat: -41.8899962, lng: 174.0492437, z: 14 }, tileSet: 'topographic', style: 'topographic' },
@@ -31,20 +31,20 @@ export class CommandScreenShot extends Command {
 
     logger.info('Page:Launch');
     const chrome = await chromium.launch();
-    const page = await chrome.newPage();
 
     try {
-      await this.takeScreenshots(page, logger);
+      await this.takeScreenshots(chrome, logger);
     } finally {
-      await page.close();
       await chrome.close();
     }
   }
 
-  async takeScreenshots(page: Page, logger: LogType): Promise<void> {
+  async takeScreenshots(chrome: Browser, logger: LogType): Promise<void> {
     const { flags } = await this.parse(CommandScreenShot);
 
     for (const test of TileTest) {
+      const page = await chrome.newPage();
+
       const tileSetId = await this.getTileSetId(test.tileSet, flags.tag);
       const styleId = await this.getStyleId(test.style, flags.tag);
 
@@ -53,7 +53,7 @@ export class CommandScreenShot extends Command {
       searchParam.set('i', tileSetId);
       if (styleId) searchParam.set('s', styleId);
 
-      const loc = `${test.location.lat},${test.location.lng},z${test.location.z}`;
+      const loc = `@${test.location.lat},${test.location.lng},z${test.location.z}`;
       const fileName = '.artifacts/visual-snapshots/' + flags.host + '_' + test.name + '.png';
 
       await mkdir(`.artifacts/visual-snapshots/`, { recursive: true });
@@ -64,16 +64,21 @@ export class CommandScreenShot extends Command {
 
       await page.goto(url);
 
-      if (flags.host.startsWith('dev')) {
-        await page.waitForSelector('div#map-loaded', { state: 'attached' });
-        await page.waitForTimeout(1000);
-        await page.waitForLoadState('networkidle');
-      } else {
-        throw new Error('Not supported on production yet');
+      try {
+        if (flags.host.startsWith('dev')) {
+          await page.waitForSelector('div#map-loaded', { state: 'attached' });
+          await page.waitForTimeout(1000);
+          await page.waitForLoadState('networkidle');
+        } else {
+          throw new Error('Not supported on production yet');
+        }
+        await page.screenshot({ path: fileName });
+      } catch (e) {
+        await page.screenshot({ path: fileName });
+        throw e;
       }
-      await page.screenshot({ path: fileName });
-
       logger.info({ url, expected: fileName }, 'Page:Load:Done');
+      await page.close();
     }
   }
 
