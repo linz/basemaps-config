13572906489c9533f28815ea93a2c4ef70fc90b9
Wentao Kuang <wkuang@linz.govt.nz>
2022-07-07 10:05:50 +1200

    Add the preview infra code and workflow.

---
+++ infra/basemaps.ts
@@ -0,0 +1,16 @@
+import { handler as h } from '@basemaps/lambda-tiler';
+
+// import { ConfigProviderMemory, Config } from '@basemaps/config';
+// import { Config } from '@basemaps/lambda-tiler';
+// import { ConfigData } from './config.js';
+
+// const mem = ConfigProviderMemory.fromJson(ConfigData);
+// Config.setConfigProvider(mem);
+
+export function handler(evt: any, context: any, cb: any): void {
+  if (evt.requestContext.domainName) {
+    process.env['BASEMAPS_PUBLIC_URL'] = 'https://' + evt.requestContext.domainName;
+  }
+
+  h(evt, context, cb);
+}
