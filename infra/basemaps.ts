d71258e71e92bf5236147d39dd834e897f89d1c1
Wentao Kuang <wkuang@linz.govt.nz>
2022-07-12 11:30:16 +1200

    Add assets to preview

--- infra/basemaps.ts
+++ infra/basemaps.ts
@@ -2,7 +2,7 @@ import { handler as h } from '@basemaps/lambda-tiler';
 import { ConfigProviderMemory, Config, ConfigBundled } from '@basemaps/config';
 import fs from 'fs';
 
-const data = fs.readFileSync('../config.json');
+const data = fs.readFileSync('./config.json');
 const json = JSON.parse(data.toString());
 const mem = ConfigProviderMemory.fromJson(json as ConfigBundled);
 Config.setConfigProvider(mem);
@@ -10,6 +10,7 @@ Config.setConfigProvider(mem);
 export function handler(evt: any, context: any, cb: any): void {
   if (evt.requestContext.domainName) {
     process.env['BASEMAPS_PUBLIC_URL'] = 'https://' + evt.requestContext.domainName;
+    process.env['BASEMAPS_ASSEST_LOCATION'] = './assets';
   }
 
   h(evt, context, cb);
