77bfd628cd7cd3d7aaffecc0186bde0780383f91
Wentao Kuang <wkuang@linz.govt.nz>
2022-07-12 11:14:25 +1200

    fix

--- infra/basemaps.ts
+++ infra/basemaps.ts
@@ -1,9 +1,10 @@
 import { handler as h } from '@basemaps/lambda-tiler';
 import { ConfigProviderMemory, Config, ConfigBundled } from '@basemaps/config';
-import { fsa } from '@chunkd/fs';
+import fs from 'fs';
 
-const configJson = await fsa.readJson<ConfigBundled>('../config.json');
-const mem = ConfigProviderMemory.fromJson(configJson);
+const data = fs.readFileSync('../config.json');
+const json = JSON.parse(data.toString());
+const mem = ConfigProviderMemory.fromJson(json as ConfigBundled);
 Config.setConfigProvider(mem);
 
 export function handler(evt: any, context: any, cb: any): void {
