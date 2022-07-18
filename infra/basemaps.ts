6b4c2045e398f530b6170a269d24c7071761d48f
Wentao Kuang <wkuang@linz.govt.nz>
2022-07-18 14:39:26 +1200

    Move read config at top level.

--- infra/basemaps.ts
+++ infra/basemaps.ts
@@ -2,18 +2,19 @@ import { handler as h } from '@basemaps/lambda-tiler';
 import { ConfigProviderMemory, Config, ConfigBundled } from '@basemaps/config';
 import { fsa } from '@chunkd/fs';
 
-const configLocation = process.env['CONFIG_LOCATION'];
 process.env['BASEMAPS_ASSEST_LOCATION'] = process.env['ASSETS_LOCATION'];
 
-export async function handler(evt: any, context: any, cb: any): Promise<void> {
+(async (): Promise<void> => {
+  const configLocation = process.env['CONFIG_LOCATION'];
   if (configLocation == null) throw new Error('Config File Not Found');
   const configJson = await fsa.readJson<ConfigBundled>(configLocation);
   const mem = ConfigProviderMemory.fromJson(configJson);
   Config.setConfigProvider(mem);
+})();
 
+export function handler(evt: any, context: any, cb: any): void {
   if (evt.requestContext.domainName) {
     process.env['BASEMAPS_PUBLIC_URL'] = 'https://' + evt.requestContext.domainName;
-    process.env['BASEMAPS_ASSEST_LOCATION'] = './assets';
   }
 
   h(evt, context, cb);
