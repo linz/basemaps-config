36a6bc30edf3715a8f6624cb7a797a708ab52949
Wentao Kuang <wkuang@linz.govt.nz>
2023-11-06 15:20:57 +1300

    Only sort styles

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,13 @@
 module.exports = {
   ...require('@linzjs/style/.prettierrc.js'),
-  printWidth: 200
+  printWidth: 200,
+  overrides: [
+    {
+      files: ["config/style/*.json"],
+      options: {
+        plugins: ["prettier-plugin-sort-json"],
+        jsonRecursiveSort: true
+      }
+    },
+  ]
 };
