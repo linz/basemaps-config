cec562656642ee95e1df6ff900e6efb1eaeb2bf8
Blayne Chard <bchard@linz.govt.nz>
2023-12-06 14:22:23 +1300

    fix: sprites is no longer a basemaps-cli cli

--- scripts/bmc.sh
+++ scripts/bmc.sh
@@ -5,5 +5,5 @@ docker run \
   -v ${PWD}:${PWD} \
   -p 5000:5000 \
   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_REGION -e AWS_DEFAULT_REGION \
-  ghcr.io/linz/basemaps/cli:v6 \
+  ghcr.io/linz/basemaps/cli:latest \
   "$@"
