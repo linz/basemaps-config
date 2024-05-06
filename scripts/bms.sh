d2f60fa483172dc3053a6aab5b3a8f54e90ecdb8
Wentao Kuang <wt.kuang@icloud.com>
2024-05-06 13:25:50 +1200

    ci: Add basemaps smoke test in the ci/cd step. BM-1010 (#891)
    #### Motivation
    
    Running basemaps smoke tests in the ci/cd.
    
    #### Modification
    
    Add another step in ci to run smoke test. 
    
    #### Checklist
    
    _If not applicable, provide explanation of why._
    
    - [ ] Tests updated
    - [ ] Docs updated
    - [ ] Issue linked in Title

--- scripts/bms.sh
+++ scripts/bms.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Run the basemaps/cli as a docker container with all the extra environment vars and args passed in
+# Specific for the basemaps server cmd with a port 5000
 docker run \
-  --entrypoint node \
+  --name bms \
   -v ${PWD}:${PWD} \
+  -p 5000:5000 \
   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_REGION -e AWS_DEFAULT_REGION -e BASEMAPS_HOST \
   --rm \
-  --network="host" \
   ghcr.io/linz/basemaps/cli:${BASEMAPS_CONTAINER_VERSION:-latest} \
   "$@"
