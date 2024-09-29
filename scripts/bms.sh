5117712880e8007c26fb5ef0feeff877c80c7697
Wentao Kuang <wkuang@linz.govt.nz>
2024-09-30 11:29:32 +1300

    Update the ci to use basemaps/server container for screenshots and smoke test

--- scripts/bms.sh
+++ scripts/bms.sh
@@ -8,5 +8,5 @@ docker run \
   -p 5000:5000 \
   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_REGION -e AWS_DEFAULT_REGION -e BASEMAPS_HOST \
   --rm \
-  ghcr.io/linz/basemaps/cli:${BASEMAPS_CONTAINER_VERSION:-latest} \
+  ghcr.io/linz/basemaps/server:${BASEMAPS_SERVER_CONTAINER_VERSION:-latest} \
   "$@"
